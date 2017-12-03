489fb7c5e77fdc56ad72a256e77dc3ba3802920e
Delyan Nikolaev Nikolov <delyan.nikolov.1992@gmail.com>
2017-12-03 16:36:19 +0200

    Extended business sectors and towns

--- JobFinder-System/JobFinder.Data/Migrations/Configuration.cs
+++ JobFinder-System/JobFinder.Data/Migrations/Configuration.cs
@@ -40,62 +40,403 @@ namespace JobFinder.Data.Migrations
             }
 
             // Add towns
+            var jobOffersPerTown = new List<JobOfferPerCriteria>
+            {
+                new JobOfferPerCriteria
+                {
+                    Name = "Blagoevgrad"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Burgas"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Dobrich"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Gabrovo"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Haskovo"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Kardzhali"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Kyustendil"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Lovech"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Montana"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Pazardzhik"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Pernik"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Pleven"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Plovdiv"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Razgrad"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Ruse"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Shumen"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Silistra"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Sliven"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Smolyan"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Sofia City"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Sofia (province)"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Stara Zagora"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Targovishte"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Varna"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Veliko Tarnovo"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Vidin"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Vratsa"
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Yambol"
+                }
+            };
+
+            var towns = jobOffersPerTown.Select(x => new Town { Name = x.Name }).ToArray();
+
             if (!context.Towns.Any())
             {
                 context.Towns.AddOrUpdate(
                     t => t.Name,
-                    new Town { Name = "Sofia" },
-                    new Town { Name = "Plovdiv" },
-                    new Town { Name = "Varna" },
-                    new Town { Name = "Burgas" },
-                    new Town { Name = "Ruse" },
-                    new Town { Name = "Vraca" },
-                    new Town { Name = "Pavlikeni" },
-                    new Town { Name = "Veliko Turnovo" },
-                    new Town { Name = "Svishtov" },
-                    new Town { Name = "Razgrad" },
-                    new Town { Name = "Shumen" },
-                    new Town { Name = "Haskovo" },
-                    new Town { Name = "Pernik" },
-                    new Town { Name = "Blagoevgrad" },
-                    new Town { Name = "Kardzhali" },
-                    new Town { Name = "Pazardjik" });
+                    towns);
             }
 
             // Add business sectors
-            var sports = new BusinessSector { Name = "Sports" };
-            var security = new BusinessSector { Name = "Security" };
-            var travel = new BusinessSector { Name = "Travel" };
+            var jobOffersPerBusinessSector = new List<JobOfferPerCriteria>
+            {
+                new JobOfferPerCriteria
+                {
+                    Name = "Accounting, Audit, Finance",
+                    Count = 1272
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Administrative and office",
+                    Count = 2462
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Advertising, PR",
+                    Count = 353
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Agricultures, Forestry & Fishing",
+                    Count = 244
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Architecture, Construction",
+                    Count = 1540
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Arts, Entertainment, Promotion",
+                    Count = 588
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Automotive, Auto Service, Gas Stations",
+                    Count = 1307
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Aviation, Airport & Airline",
+                    Count = 55
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Banking, Lending",
+                    Count = 892
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Beauty, SPA & Salon",
+                    Count = 492
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Business/Consultancy services",
+                    Count = 765
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Cleaning, Household services",
+                    Count = 1062
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Contact centers (Call centers)",
+                    Count = 1080
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Design, Creative, Video & Animation",
+                    Count = 360
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Drivers, Couriers",
+                    Count = 1828
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Education, Courses, Translators",
+                    Count = 594
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Energy and Utilities",
+                    Count = 429
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Engineers",
+                    Count = 1278
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Healthcare and pharmacy",
+                    Count = 1041
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Hotels",
+                    Count = 1523
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Human Resources",
+                    Count = 282
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Installation, Maintenance and Repair",
+                    Count = 1452
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Insurance",
+                    Count = 
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "IT - Administration and sales",
+                    Count = 333
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "IT - Hardware design/maintenance",
+                    Count = 563
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "IT - Software development/maintenance",
+                    Count = 1892
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Legal",
+                    Count = 122
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Management, Business development",
+                    Count = 492
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Manual work",
+                    Count = 804
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Manufacturing - Electronics, Electrical, Mechanical",
+                    Count = 1115
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Manufacturing - Chemistry and Oil",
+                    Count = 120
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Manufacturing - Food and Beverage",
+                    Count = 1111
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Manufacturing - Furniture and Carpentry",
+                    Count = 197
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Manufacturing - Pharmaceutical",
+                    Count = 70
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Manufacturing - Textile and Clothes",
+                    Count = 580
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Manufacturing - Ìetallurgy and Mining",
+                    Count = 187
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Manufacturing - Other",
+                    Count = 2056
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Marine and Shipping",
+                    Count = 37
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Marketing",
+                    Count = 831
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Media, Publishing",
+                    Count = 170
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "NGO",
+                    Count = 60
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Public sector, Government",
+                    Count = 193
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Real-estate",
+                    Count = 276
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Research and development",
+                    Count = 174
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Restaurants and Catering",
+                    Count = 3420
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Retail, Wholesale",
+                    Count = 6926
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Security",
+                    Count = 511
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Sports, Kinesiotherapy",
+                    Count = 210
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Telecommunications - Administration and sales",
+                    Count = 447
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Telecommunications - engineers and technicians",
+                    Count = 327
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Transport, Logistics, Spedition",
+                    Count = 1822
+                },
+                new JobOfferPerCriteria
+                {
+                    Name = "Travel and Tourism",
+                    Count = 261
+                }
+            };
+
+            var businessSectors = jobOffersPerBusinessSector.Select(x => new BusinessSector { Name = x.Name }).ToArray();
 
             if (!context.BusinessSectors.Any())
             {
                 context.BusinessSectors.AddOrUpdate(
                     s => s.Name,
-                    new BusinessSector { Name = "Accounting" },
-                    new BusinessSector { Name = "Finance" },
-                    new BusinessSector { Name = "Advertising/PR" },
-                    new BusinessSector { Name = "Auto Service" },
-                    new BusinessSector { Name = "Aviation" },
-                    new BusinessSector { Name = "Banking" },
-                    new BusinessSector { Name = "Contact centers" },
-                    new BusinessSector { Name = "Design" },
-                    new BusinessSector { Name = "Drivers" },
-                    new BusinessSector { Name = "Education" },
-                    new BusinessSector { Name = "Engineers" },
-                    new BusinessSector { Name = "Healthcare" },
-                    new BusinessSector { Name = "Human Resources" },
-                    new BusinessSector { Name = "Software development" },
-                    new BusinessSector { Name = "IT - Hardware" },
-                    new BusinessSector { Name = "Insurance" },
-                    new BusinessSector { Name = "Logistics" },
-                    new BusinessSector { Name = "Management" },
-                    new BusinessSector { Name = "Marketing" },
-                    new BusinessSector { Name = "Production" },
-                    new BusinessSector { Name = "Food & Beverage" },
-                    new BusinessSector { Name = "Telecommunications" },
-                    sports,
-                    security,
-                    travel);
+                    businessSectors);
             }
 
             // Create admin, company and person
@@ -120,7 +461,7 @@ namespace JobFinder.Data.Migrations
                     {
                         Bulstat = "1234567891234",
                         Email = "aaaaaa@abv.bg",
-                        BusinessSectors = new HashSet<BusinessSector> { sports, travel },
+                        BusinessSectors = new HashSet<BusinessSector>(businessSectors.Take(2)),
                         UserName = "aaaaaa@abv.bg",
                         CompanyName = "Abc",
                         IsApproved = true,
@@ -137,7 +478,7 @@ namespace JobFinder.Data.Migrations
                     {
                         Bulstat = "2234567891234",
                         Email = "company@abv.bg",
-                        BusinessSectors = new HashSet<BusinessSector> { security },
+                        BusinessSectors = new HashSet<BusinessSector>(businessSectors.Skip(2).Take(1)),
                         UserName = "company@abv.bg",
                         CompanyName = "B&B",
                         IsApproved = true,
@@ -162,6 +503,19 @@ namespace JobFinder.Data.Migrations
                     manager.AddToRole(person.Id, "Person");
                 }
             }
+
+            // Create job offers
+            if (!context.JobOffers.Any())
+            {
+
+            }
+        }
+
+        private class JobOfferPerCriteria
+        {
+            public string Name { get; set; }
+
+            public int Count { get; set; }
         }
     }
 }
