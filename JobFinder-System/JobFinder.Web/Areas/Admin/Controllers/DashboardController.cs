df3b1675fa5ff3bafe93c9108d6d6aa1ad705529
Delyan Nikolaev Nikolov <delyan.nikolov.1992@gmail.com>
2017-12-09 17:05:20 +0200

    Added some menu for dashboard

--- JobFinder-System/JobFinder.Web/Areas/Admin/Controllers/DashboardController.cs
+++ JobFinder-System/JobFinder.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -6,6 +6,9 @@
     using JobFinder.Data;
     using JobFinder.Web.Areas.Admin.Models.DashboardModels;
     using JobFinder.Web.Controllers;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Newtonsoft.Json.Serialization;
 
     [Authorize(Roles = "Admin")]
     public class DashboardController : BaseController
@@ -31,6 +34,35 @@
                 .Select(o => new DashboardViewModel { Name = o.Name, Y = Math.Round(Convert.ToDouble(o.Y / jobOffersCount) * 100, 2) });
 
             return this.View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Dido()
+        {
+            var jobOffersCount = this.Data.JobOffers.All().Where(o => o.IsActive).Count();
+
+            if (jobOffersCount == 0)
+            {
+                return this.Json(new object[] { });
+            }
+
+            var model = this.Data.JobOffers.All().Where(o => o.IsActive).GroupBy(o => o.BusinessSectorId)
+                .Select(o => new DashboardViewModel { Name = o.FirstOrDefault().BusinessSector.Name, Y = o.Count() })
+                .ToList()
+                .Select(o => new DashboardViewModel { Name = o.Name, Y = Math.Round(Convert.ToDouble(o.Y / jobOffersCount) * 100, 2) })
+                .ToList();
+
+            return this.Json(this.ToJson(model));
+        }
+
+        private object ToJson(object obj)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            settings.Converters.Add(new JavaScriptDateTimeConverter());
+            return JsonConvert.SerializeObject(obj, settings);
         }
     }
 }
